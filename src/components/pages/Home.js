9392c8844a58ceefbb5b419db5af7bcfce80c443
arthur-lage <arthurlage2006@gmail.com>
2021-03-19 22:21:09 -0300

    New pages, new footer, cards, etc

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,16 @@
 import '../../App.css'
 import React from 'react'
 import HeroSection from '../HeroSection'
+import Cards from '../Cards';
+import Footer from '../Footer';
 
 function Home(){
     return(
-        <HeroSection />
+        <>
+            <HeroSection />
+            <Cards />
+            <Footer />
+        </>
     )
 }
 
